97d90b4c18bc77c8c73b4eb0db9cf5cd5c938738
polizz_v <polizzi.valerian@gmail.com>
2014-04-01 13:10:23 +0200

    list added

---
+++ CPE_2014_corewar/list_d/list.h
@@ -0,0 +1,25 @@
+/*
+** list.h for list in /home/polizz_v/rendu/libmy
+** 
+** Made by valeri
+** Login   <polizz_v@epitech.net>
+** 
+** Started on  Mon Mar 31 14:50:32 2014 valeri
+** Last update Tue Apr  1 12:44:16 2014 valeri
+*/
+
+#ifndef LIST_H_
+# define LIST_H_
+
+typedef struct		s_list
+{
+  char			*data;
+  struct s_list		*next;
+  struct s_list		*prev;
+}			t_list;
+
+int			my_put_in_list_first(t_list **list, char *data);
+int			my_put_in_list_last(t_list **list, char *data);
+int			my_show_list(t_list *list);
+
+#endif
